d2576e26077672fd5550c8c347e2d564a825b04a
DanBel <dan.bel.89@bk.ru>
2026-05-07 00:58:19 +0400

    update 2026-05-07 00:58:06

--- tukhtarov-api/src/main/java/com/github/danbel/tukhtarovapi/config/CorsConfig.java
+++ tukhtarov-api/src/main/java/com/github/danbel/tukhtarovapi/config/CorsConfig.java
@@ -14,12 +14,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(
-                                "http://localhost:3000",
-                                "http://127.0.0.1:3000"
-                        )
+                        .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
+                        .exposedHeaders("*")
                         .allowCredentials(false);
             }
         };
