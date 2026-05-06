582348a30e23fe1840516d79ed2e1eb55b302368
DanBel <dan.bel.89@bk.ru>
2026-05-07 02:49:15 +0400

    update 2026-05-07 02:49:02

--- tukhtarov-api/src/main/java/com/github/danbel/tukhtarovapi/repository/AppUserRepository.java
+++ tukhtarov-api/src/main/java/com/github/danbel/tukhtarovapi/repository/AppUserRepository.java
@@ -4,11 +4,20 @@ import com.github.danbel.tukhtarovapi.domain.entity.AppUser;
 import com.github.danbel.tukhtarovapi.domain.enumtype.UserRole;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
 
+    @EntityGraph(attributePaths = "clientCompany")
+    List<AppUser> findAll();
+
+    @EntityGraph(attributePaths = "clientCompany")
     List<AppUser> findByRoleOrderByFullNameAsc(UserRole role);
 
+    @EntityGraph(attributePaths = "clientCompany")
     Optional<AppUser> findByLoginIgnoreCaseOrEmailIgnoreCase(String login, String email);
+
+    @EntityGraph(attributePaths = "clientCompany")
+    Optional<AppUser> findById(Long id);
 }
