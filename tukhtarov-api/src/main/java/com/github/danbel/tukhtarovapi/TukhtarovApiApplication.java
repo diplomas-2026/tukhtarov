5a43f1a278d310e897e92cbf1818a3d096e3a815
DanBel <dan.bel.89@bk.ru>
2026-05-06 23:33:23 +0400

    update

---
+++ tukhtarov-api/src/main/java/com/github/danbel/tukhtarovapi/TukhtarovApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.danbel.tukhtarovapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TukhtarovApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TukhtarovApiApplication.class, args);
+    }
+
+}
---
+++ tukhtarov-api/src/test/java/com/github/danbel/tukhtarovapi/TukhtarovApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.danbel.tukhtarovapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TukhtarovApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
